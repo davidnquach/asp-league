2f6b7ad75e7aefdff3104165dc0e047f447e4798
David Quach <davidquach@MacBook-Pro-2.local>
2016-09-20 20:35:32 -0400

    Load fixtures in rspec tests

--- spec/models/rate_limit_spec.rb
+++ spec/models/rate_limit_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe RateLimit, type: :model do
   describe '#buffer_limit' do
+    fixtures :rate_limit
+
     it 'returns true when limit is about 80%' do
       limit = 500
       partial_limit = (limit * 0.85).to_i
